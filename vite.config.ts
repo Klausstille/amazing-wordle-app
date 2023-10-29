a593fb019c4d8a327c62b654e2b411a75c8e8b4a
KlausStille <mail@klausstille.com>
2023-10-29 17:23:31 +0100

    /fix baseURL

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    // base: "/amazing-wordle-app/",
-    // server: {
-    //     proxy: {
-    //         "/api": {
-    //             target: "https://api.pons.com",
-    //             changeOrigin: true,
-    //             rewrite: (path) => path.replace(/^\/api/, ""),
-    //         },
-    //     },
-    // },
+    base: "/",
+    server: {
+        proxy: {
+            "/api": {
+                target: "https://api.pons.com",
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ""),
+            },
+        },
+    },
 });
