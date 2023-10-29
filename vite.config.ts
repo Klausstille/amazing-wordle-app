aa299cf5379e0c8a7da841593ea31f462f7a661f
KlausStille <mail@klausstille.com>
2023-10-29 18:03:34 +0100

    added proxy bc of CORS policy

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    // base: "/",
-    // server: {
-    //     proxy: {
-    //         "/api": {
-    //             target: "https://api.pons.com",
-    //             changeOrigin: true,
-    //             rewrite: (path) => path.replace(/^\/api/, ""),
-    //         },
-    //     },
-    // },
+    server: {
+        port: 3000,
+        proxy: {
+            "/api": {
+                target: "https://api.pons.com/",
+                changeOrigin: true,
+                pathRewrite: {
+                    "^/api": "", // Adjust path rewrite as needed
+                },
+            },
+        },
+    },
 });
