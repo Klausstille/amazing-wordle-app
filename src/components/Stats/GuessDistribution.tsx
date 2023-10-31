c1366d339411565e304be0731a77d4538862e3ec
KlausStille <mail@klausstille.com>
2023-10-31 16:22:02 +0100

    refactored guess distribution

--- src/components/Stats/GuessDistribution.tsx
+++ src/components/Stats/GuessDistribution.tsx
@@ -11,68 +11,38 @@ export default function GuessDistribution({
 }: GuessDistributionProps) {
     if (!stats) return null;
 
-    const wonGames = stats?.filter((game) => game.isWin);
-    const distribution = wonGames?.map((game) => game.game.length);
+    const wonGames = stats.filter((game) => game.isWin);
+    const distribution = wonGames.map((game) => game.game.length);
 
-    const width = (idx: number) => {
-        const distributionLength = distribution?.filter((num) => num === idx);
-        const result = (distributionLength?.length / wonGames?.length) * 400;
-        const relativeWidth = Math.min(250, result);
-        // console.log(idx, Math.round(relativeWidth));
-        return Math.round(relativeWidth);
+    const calculateWidth = (idx: number) => {
+        const distributionLength = distribution.filter((num) => num === idx);
+        const result = (distributionLength.length / wonGames.length) * 400;
+        return Math.min(250, Math.round(result));
     };
+
+    const title = lang === "en" ? "GUESS DISTRIBUTION" : "SCHÄTZVERTEILUNG";
 
     return (
         <>
-            {lang === "en" ? (
-                <>
-                    <h3 className="font-black pt-6">GUESS DISTRIBUTION</h3>
-                    <section className="grid gap-1">
-                        <section className="grid gap-1">
-                            {Array.from({ length: 6 }).map((_, index) => {
-                                return (
-                                    <aside
-                                        key={index}
-                                        style={{
-                                            gridTemplateColumns: `20px ${width(
-                                                index + 1
-                                            )}px`,
-                                        }}
-                                        className={`grid`}
-                                    >
-                                        <p>{index + 1}</p>
-                                        <span className="bg-[#4456e1]"></span>
-                                    </aside>
-                                );
-                            })}
-                        </section>
-                    </section>
-                </>
-            ) : (
-                <>
-                    <h3 className="font-black pt-6">SCHÄTZVERTEILUNG</h3>
-                    <section className="grid gap-1">
-                        <section className="grid gap-1">
-                            {Array.from({ length: 6 }).map((_, index) => {
-                                return (
-                                    <aside
-                                        key={index}
-                                        style={{
-                                            gridTemplateColumns: `20px ${width(
-                                                index + 1
-                                            )}px`,
-                                        }}
-                                        className={`grid`}
-                                    >
-                                        <p>{index + 1}</p>
-                                        <span className="bg-[#4456e1]"></span>
-                                    </aside>
-                                );
-                            })}
-                        </section>
-                    </section>
-                </>
-            )}
+            <h3 className="font-black pt-6">{title}</h3>
+            <section className="grid gap-1">
+                <section className="grid gap-1">
+                    {Array.from({ length: 6 }).map((_, index) => (
+                        <aside
+                            key={index}
+                            style={{
+                                gridTemplateColumns: `20px ${calculateWidth(
+                                    index + 1
+                                )}px`,
+                            }}
+                            className={`grid`}
+                        >
+                            <p>{index + 1}</p>
+                            <span className="bg-[#4456e1]"></span>
+                        </aside>
+                    ))}
+                </section>
+            </section>
         </>
     );
 }
