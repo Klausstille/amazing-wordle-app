fd882ebb28e196628211bba6d723e8a2d8871340
KlausStille <mail@klausstille.com>
2023-10-31 19:54:25 +0100

    refactored stats page

--- src/components/Stats/Statistics.tsx
+++ src/components/Stats/Statistics.tsx
@@ -1,6 +1,5 @@
 import { Stats } from "../../App";
-import { calculateCurrentStreak } from "./calculateCurrentStreak";
-import { calculateMaxStreak } from "./calculateMaxStreak";
+import { calculateMaxStreak, calculateCurrentStreak } from "./calculateStreak";
 
 interface StatisticsProps {
     stats: Stats[];
@@ -8,90 +7,59 @@ interface StatisticsProps {
     lang: string;
 }
 
-export default function Statistics({ stats, player, lang }: StatisticsProps) {
+export default function Statistics({
+    stats,
+    player,
+    lang,
+}: StatisticsProps): JSX.Element {
     const currentStreak = calculateCurrentStreak(stats);
     const maxStreak = calculateMaxStreak(stats);
+
+    const isEnglish = lang === "en";
+
+    const message = isEnglish ? "Good game!" : "Viel Spaß!";
+    const gamesLabel = isEnglish ? "Played" : "Spiele";
+    const winPercentageLabel = isEnglish ? "Win %" : "Gewonnen %";
+    const currentStreakLabel = isEnglish ? "Current Streak" : "Laufende Abf.";
+    const maxStreakLabel = isEnglish ? "Max Streak" : "Maximale Abf.";
+
     return (
         <>
-            {lang === "en" ? (
-                <>
-                    {stats.length <= 1 && (
-                        <h2 className="font-black pt-8 text-center">
-                            Hi {player}
-                            <p>Good game!</p>
-                        </h2>
-                    )}
-                    <h3 className="font-black pt-8">STATISTICS</h3>
-                    <section className="grid grid-cols-4 gap-3">
-                        <aside className="text-center">
-                            <h2>{stats.length - 1}</h2>
-                            <p>Played</p>
-                        </aside>
-                        <aside className="text-center">
-                            {stats.length > 1 ? (
-                                <h2>
-                                    {Math.round(
-                                        (stats?.filter((stat) => stat?.isWin)
-                                            .length /
-                                            (stats.length - 1)) *
-                                            100
-                                    )}
-                                </h2>
-                            ) : (
-                                <h2>0</h2>
-                            )}
-                            <p>Win %</p>
-                        </aside>
-                        <aside className="text-center">
-                            <h2>{currentStreak}</h2>
-                            <p>Current Streak</p>
-                        </aside>
-                        <aside className="text-center">
-                            <h2>{maxStreak} </h2>
-                            <p>Max Streak</p>
-                        </aside>
-                    </section>
-                </>
-            ) : (
-                <>
-                    {stats.length <= 1 && (
-                        <h2 className="font-black pt-8 text-center">
-                            Hallo {player}
-                            <p>Viel Spaß!</p>
-                        </h2>
-                    )}
-                    <h3 className="font-black pt-8">STATISTICS</h3>
-                    <section className="grid grid-cols-4 gap-3">
-                        <aside className="text-center">
-                            <h2>{stats.length - 1}</h2>
-                            <p>Spiele</p>
-                        </aside>
-                        <aside className="text-center">
-                            {stats.length > 1 ? (
-                                <h2>
-                                    {Math.round(
-                                        (stats?.filter((stat) => stat?.isWin)
-                                            .length /
-                                            (stats.length - 1)) *
-                                            100
-                                    )}
-                                </h2>
-                            ) : (
-                                <h2>0</h2>
-                            )}
-                            <p>Gewonnen %</p>
-                        </aside>
-                        <aside className="text-center">
-                            <h2>{currentStreak}</h2>
-                            <p>Laufende Abf.</p>
-                        </aside>
-                        <aside className="text-center">
-                            <h2>{maxStreak} </h2>
-                            <p>Maximale Abf.</p>
-                        </aside>
-                    </section>
-                </>
+            {stats.length <= 1 && (
+                <h2 className="font-black pt-8 text-center">
+                    {isEnglish ? `Hi ${player}` : `Hallo ${player}`}
+                    <p>{message}</p>
+                </h2>
             )}
+            <h3 className="font-black pt-8">STATISTICS</h3>
+            <section className="grid grid-cols-4 gap-3">
+                <aside className="text-center">
+                    <h2>{stats.length - 1}</h2>
+                    <p>{gamesLabel}</p>
+                </aside>
+                <aside className="text-center">
+                    {stats.length > 1 ? (
+                        <h2>
+                            {Math.round(
+                                (stats?.filter((stat) => stat?.isWin).length /
+                                    (stats.length - 1)) *
+                                    100
+                            )}
+                        </h2>
+                    ) : (
+                        <h2>0</h2>
+                    )}
+                    <p>{winPercentageLabel}</p>
+                </aside>
+                <aside className="text-center">
+                    <h2>{currentStreak}</h2>
+                    <p>{currentStreakLabel}</p>
+                </aside>
+                <aside className="text-center">
+                    <h2>{maxStreak}</h2>
+                    <p>{maxStreakLabel}</p>
+                </aside>
+            </section>
         </>
     );
 }
--- src/components/Stats/calculateStreak.tsx
+++ src/components/Stats/calculateStreak.tsx
@@ -1,5 +1,22 @@
 import { Stats } from "../../App";
 
+export const calculateMaxStreak = (stats: Stats[]) => {
+    let maxStreak = 0;
+    let currentStreakCount = 0;
+
+    for (const stat of stats) {
+        if (stat?.isWin) {
+            currentStreakCount++;
+            if (currentStreakCount > maxStreak) {
+                maxStreak = currentStreakCount;
+            }
+        } else {
+            currentStreakCount = 0;
+        }
+    }
+    return maxStreak;
+};
+
 export const calculateCurrentStreak = (stats: Stats[]) => {
     let currentStreak = 0;
     let currentStreakCount = 0;
