3d97430367c3bf6b63e7b2a2db98fac1acd55df3
KlausStille <mail@klausstille.com>
2023-11-03 16:18:23 +0100

    updated guess distribution to include the number of games won

--- src/components/Stats/GuessDistribution.tsx
+++ src/components/Stats/GuessDistribution.tsx
@@ -17,7 +17,9 @@ export default function GuessDistribution({
     const calculateWidth = (idx: number) => {
         const distributionLength = distribution.filter((num) => num === idx);
         const result = (distributionLength.length / wonGames.length) * 400;
-        return Math.min(250, Math.round(result));
+        const widthOfElement = Math.min(250, Math.round(result));
+        const numberOfWonGames = distributionLength.length;
+        return { widthOfElement, numberOfWonGames };
     };
 
     const title = lang === "en" ? "GUESS DISTRIBUTION" : "SCHÄTZVERTEILUNG";
@@ -27,20 +29,24 @@ export default function GuessDistribution({
             <h3 className="font-black pt-6">{title}</h3>
             <section className="grid gap-1">
                 <section className="grid gap-1">
-                    {Array.from({ length: 6 }).map((_, index) => (
-                        <aside
-                            key={index}
-                            style={{
-                                gridTemplateColumns: `20px ${calculateWidth(
-                                    index + 1
-                                )}px`,
-                            }}
-                            className={`grid`}
-                        >
-                            <p>{index + 1}</p>
-                            <span className="bg-[#4456e1]"></span>
-                        </aside>
-                    ))}
+                    {Array.from({ length: 6 }).map((_, index) => {
+                        const { widthOfElement, numberOfWonGames } =
+                            calculateWidth(index + 1);
+                        return (
+                            <aside
+                                key={index}
+                                style={{
+                                    gridTemplateColumns: `20px ${widthOfElement}px`,
+                                }}
+                                className={`grid`}
+                            >
+                                <p>{index + 1}</p>
+                                <span className="bg-[#4456e1] text-s text-right pr-1 min-w-[15px]">
+                                    <p> {numberOfWonGames}</p>
+                                </span>
+                            </aside>
+                        );
+                    })}
                 </section>
             </section>
         </>
