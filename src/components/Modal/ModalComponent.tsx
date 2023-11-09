7dcb2b1c4805597b95ab360acb22b62322c56801
KlausStille <mail@klausstille.com>
2023-11-09 15:35:01 +0100

    /fix css

--- src/components/Modal/ModalComponent.tsx
+++ src/components/Modal/ModalComponent.tsx
@@ -1,5 +1,4 @@
 import { Hint } from "../../App";
-import * as React from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
