0bef37579282c385fdb32d57b0b564c9b7e667dc
KlausStille <mail@klausstille.com>
2023-11-09 15:30:32 +0100

    /fix css

--- src/components/Modal/ModalComponent.tsx
+++ src/components/Modal/ModalComponent.tsx
@@ -48,17 +48,16 @@ export default function ModalComponent({
         "*****"
     );
 
-    const style: React.CSSProperties = {
+    const style = {
         position: "absolute",
         top: "50%",
         left: "50%",
         transform: "translate(-50%, -50%)",
         width: 400,
-        border: "2px solid #000",
-        boxShadow: "24px",
-        padding: 4,
+        bgcolor: "background.paper",
+        boxShadow: 24,
+        p: 4,
         textAlign: "center",
-        backgroundColor: "black",
     };
 
     return (
