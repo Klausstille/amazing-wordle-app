8ebb15c2b4103c09f4d1bf4094519d6b6e26f702
KlausStille <mail@klausstille.com>
2023-12-01 18:25:04 +0100

    /feature spinner

--- src/components/Modal/ModalComponent.tsx
+++ src/components/Modal/ModalComponent.tsx
@@ -52,7 +52,7 @@ export default function ModalComponent({
         top: "50%",
         left: "50%",
         transform: "translate(-50%, -50%)",
-        width: 400,
+        width: 250,
         bgcolor: "background.paper",
         boxShadow: 24,
         p: 2,
